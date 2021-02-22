ad66be2c87cb5c680fb03bf2486e91d2fa8baffa
jenijr <j4316036@gmail.com>
2021-02-22 16:10:49 +0530

    my music player

--- main.js
+++ main.js
@@ -1,19 +1,109 @@
 SC.initialize({
-    client_id: '340f063c670272fac27cfa67bffcafc4'
-  });
-  $(document).ready(function() { 
-      SC.stream('/tracks/313280467',function(sound){
-        $('#start').click(function(e) {
-            e.preventDefault();
-            sound.start();
-          });
-        $('#stop').click(function(e){
-            e.preventDefault();
-            sound.stop();
-        });
-       }); 
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+$(document).ready(function () {
+  SC.stream('/tracks/73473040', function (sound) {
+    $('#start').click(function (e) {
+      e.preventDefault();
+      sound.start();
     });
+    $('#stop').click(function (e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+  SC.stream('/tracks/34304618', function (sound) {
+    $('#aa').click(function (e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#ab').click(function (e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+  SC.stream('/tracks/79623365', function (sound) {
+    $('#bb').click(function (e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#bc').click(function (e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+  SC.stream('/tracks/517722948', function (sound) {
+    $('#cc').click(function (e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#cd').click(function (e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+  SC.stream('/tracks/498231915', function (sound) {
+    $('#dd').click(function (e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#de').click(function (e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+  SC.stream('/tracks/345946410', function (sound) {
+    $('#ee').click(function (e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#ef').click(function (e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+  SC.stream('/tracks/28463385', function (sound) {
+    $('#ff').click(function (e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#fg').click(function (e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+  SC.stream('/tracks/925518151', function (sound) {
+    $('#gg').click(function (e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#gh').click(function (e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+  SC.stream('/tracks/173826641', function (sound) {
+    $('#hh').click(function (e) {
+      e.preventDefault();
+      sound.start();
+    });
+    $('#hi').click(function (e) {
+      e.preventDefault();
+      sound.stop();
+    });
+  });
+});
 
-    // <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1028720296&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/vipanchika-v" title="VIPANCHIKA V" target="_blank" style="color: #cccccc; text-decoration: none;">VIPANCHIKA V</a> · <a href="https://soundcloud.com/vipanchika-v/sets/vaanam-than-vizhunthalum" title="vaanam than vizhunthalum" target="_blank" style="color: #cccccc; text-decoration: none;">vaanam than vizhunthalum</a></div>
-    // <iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1028720296&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/vipanchika-v" title="VIPANCHIKA V" target="_blank" style="color: #cccccc; text-decoration: none;">VIPANCHIKA V</a> · <a href="https://soundcloud.com/vipanchika-v/sets/vaanam-than-vizhunthalum" title="vaanam than vizhunthalum" target="_blank" style="color: #cccccc; text-decoration: none;">vaanam than vizhunthalum</a></div>
-    // <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/966127855&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/theabs" title="curdriceboi" target="_blank" style="color: #cccccc; text-decoration: none;">curdriceboi</a> · <a href="https://soundcloud.com/theabs/master-the-blaster-ringtone" title="Master the blaster - Ringtone - Free download" target="_blank" style="color: #cccccc; text-decoration: none;">Master the blaster - Ringtone - Free download</a></div>
+// <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1028720296&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/vipanchika-v" title="VIPANCHIKA V" target="_blank" style="color: #cccccc; text-decoration: none;">VIPANCHIKA V</a> · <a href="https://soundcloud.com/vipanchika-v/sets/vaanam-than-vizhunthalum" title="vaanam than vizhunthalum" target="_blank" style="color: #cccccc; text-decoration: none;">vaanam than vizhunthalum</a></div>
+// <iframe width="100%" height="450" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1028720296&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/vipanchika-v" title="VIPANCHIKA V" target="_blank" style="color: #cccccc; text-decoration: none;">VIPANCHIKA V</a> · <a href="https://soundcloud.com/vipanchika-v/sets/vaanam-than-vizhunthalum" title="vaanam than vizhunthalum" target="_blank" style="color: #cccccc; text-decoration: none;">vaanam than vizhunthalum</a></div>
+// <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/966127855&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/theabs" title="curdriceboi" target="_blank" style="color: #cccccc; text-decoration: none;">curdriceboi</a> · <a href="https://soundcloud.com/theabs/master-the-blaster-ringtone" title="Master the blaster - Ringtone - Free download" target="_blank" style="color: #cccccc; text-decoration: none;">Master the blaster - Ringtone - Free download</a></div>
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/73473040&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/user946754676" title="Anirudh Music" target="_blank" style="color: #cccccc; text-decoration: none;">Anirudh Music</a> · <a href="https://soundcloud.com/user946754676/kanave-kanave-anirudh" title="KANAVE KANAVE - ANIRUDH" target="_blank" style="color: #cccccc; text-decoration: none;">KANAVE KANAVE - ANIRUDH</a></div>
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/163621565&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/pawanism-1" title="AKILN9" target="_blank" style="color: #cccccc; text-decoration: none;">AKILN9</a> · <a href="https://soundcloud.com/pawanism-1/vaadi-pulla-vaadi" title="Vaadi Pulla Vaadi" target="_blank" style="color: #cccccc; text-decoration: none;">Vaadi Pulla Vaadi</a></div>
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/79623365&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/prettika-vijay-chellam" title="Thivyanjana" target="_blank" style="color: #cccccc; text-decoration: none;">Thivyanjana</a> · <a href="https://soundcloud.com/prettika-vijay-chellam/vizhiyile-en-vizhiyile" title="Vizhiyile En Vizhiyile" target="_blank" style="color: #cccccc; text-decoration: none;">Vizhiyile En Vizhiyile</a></div>
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/517722948&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/arrtamilsong1" title="A.R.Rahman Tamil Songs" target="_blank" style="color: #cccccc; text-decoration: none;">A.R.Rahman Tamil Songs</a> · <a href="https://soundcloud.com/arrtamilsong1/enna-solla-pogirai" title="Enna Solla Pogirai" target="_blank" style="color: #cccccc; text-decoration: none;">Enna Solla Pogirai</a></div>
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/498231915&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/parthiban-govindasamy" title="parthiban govindasamy" target="_blank" style="color: #cccccc; text-decoration: none;">parthiban govindasamy</a> · <a href="https://soundcloud.com/parthiban-govindasamy/maruvarthai-pesathe-enai-nokki" title="Maruvarthai Pesathe - Enai Nokki Paayum Thotta" target="_blank" style="color: #cccccc; text-decoration: none;">Maruvarthai Pesathe - Enai Nokki Paayum Thotta</a></div>
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/345946410&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/arrsong" title="Ar Rahman Tamil Songs" target="_blank" style="color: #cccccc; text-decoration: none;">Ar Rahman Tamil Songs</a> · <a href="https://soundcloud.com/arrsong/munbe-vaa" title="Munbe Vaa" target="_blank" style="color: #cccccc; text-decoration: none;">Munbe Vaa</a></div>
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/28463385&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/saravanan-kumar" title="saravanan kumar" target="_blank" style="color: #cccccc; text-decoration: none;">saravanan kumar</a> · <a href="https://soundcloud.com/saravanan-kumar/kadhal-valarthen" title="Kadhal valarthen" target="_blank" style="color: #cccccc; text-decoration: none;">Kadhal valarthen</a></div>
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/925518151&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/rupesh-516854727" title="roops" target="_blank" style="color: #cccccc; text-decoration: none;">roops</a> · <a href="https://soundcloud.com/rupesh-516854727/oh-maname-masstamilan-com" title="Oh Maname - MassTamilan.com" target="_blank" style="color: #cccccc; text-decoration: none;">Oh Maname - MassTamilan.com</a></div>
+//<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/173826641&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/jason-bourne-37" title="Jason bourne 36" target="_blank" style="color: #cccccc; text-decoration: none;">Jason bourne 36</a> · <a href="https://soundcloud.com/jason-bourne-37/uthama-puthiran-kan-irandil" title="Uthama Puthiran - Kan Irandil" target="_blank" style="color: #cccccc; text-decoration: none;">Uthama Puthiran - Kan Irandil</a></div>
+<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/34304618&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/shan-12" title="Shan - Reloaded" target="_blank" style="color: #cccccc; text-decoration: none;">Shan - Reloaded</a> · <a href="https://soundcloud.com/shan-12/dhimu-dhimu-engeyum-kathal" title="Dhimu Dhimu  - Engeyum kathal" target="_blank" style="color: #cccccc; text-decoration: none;">Dhimu Dhimu  - Engeyum kathal</a></div>
